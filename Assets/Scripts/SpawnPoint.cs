fb6e5880b76992d7c728b529b691e2b9d98f7b28
Eryk <rcksorto@yahoo.com>
2018-09-22 21:21:05 -0700

    Spawn enemies from different points in map

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    private GameObject _enemy;
+
+    public GameObject enemyPrefab;
+
+    void Update()
+    {
+        if (_enemy == null)
+        {
+            _enemy = Instantiate(enemyPrefab) as GameObject;
+            _enemy.transform.position = transform.position;
+            _enemy.transform.Rotate(0, Random.Range(0, 360), 0);
+        }
+    }
+}
