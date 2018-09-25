e462e0410e9f60c59ce5e792fcc2fad8f38c374c
Eryk <rcksorto@yahoo.com>
2018-09-24 17:24:44 -0700

    Teleport player to random locations
    Locations are predefined

---
+++ Assets/Scripts/Hotspot.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Hotspot : MonoBehaviour
+{
+    public GameObject[] points;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.name.Equals("Player"))
+        {
+            collider.gameObject.transform.position = points[Random.Range(0, points.Length)].transform.position;
+        }
+    }
+}
