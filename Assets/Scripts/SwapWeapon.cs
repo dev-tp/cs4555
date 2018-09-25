a2ae554f70d014048241fafdd6aab85633ee7470
Eryk <rcksorto@yahoo.com>
2018-09-24 19:13:57 -0700

    Switch weapons without swapping animation

---
+++ Assets/Scripts/SwapWeapon.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SwapWeapon : MonoBehaviour
+{
+    public int selectedWeapon = 0;
+
+    private void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(selectedWeapon == i);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(1))
+        {
+            transform.GetChild(selectedWeapon % transform.childCount).gameObject.SetActive(false);
+            transform.GetChild(++selectedWeapon % transform.childCount).gameObject.SetActive(true);
+        }
+    }
+}
