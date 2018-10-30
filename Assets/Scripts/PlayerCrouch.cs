fb0008bec8dcf5d8bcf8d7986afe388d0cf2a639
Eryk <rcksorto@yahoo.com>
2018-10-29 20:04:15 -0700

    Player can duck/crouch

---
+++ Assets/Scripts/PlayerCrouch.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerCrouch : MonoBehaviour
+{
+    private CharacterController characterController;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        characterController.height = Input.GetKey(KeyCode.C) ? 0.8f : 1.8f;
+    }
+}
